44f9b0ee8a483f77843e176a25132afc4433d343
genBTC <genBTC@gmx.com>
2025-01-23 23:17:34 -0500

    v0.1.1 parallel processing par_iter with rayon

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-//hash_and_xattr v0.1 - 2025 - copyright genr8eofl 
+//hash_and_xattr v0.11 - 2025 (c) genr8eofl @ gmx
 use std::{fs::File, io::{self, Read}, path::Path};
 use sha2::{Sha512, Digest};
 use walkdir::WalkDir;
 use xattr::set;
+use rayon::prelude::*;
 
 fn hash_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
     let mut file = File::open(path)?;
@@ -22,40 +23,45 @@ fn hash_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
     Ok(format!("{:x}", hasher.finalize()))
 }
 
-fn set_xattr<P: AsRef<Path>>(path: P, hash: &str) -> io::Result<()> {
-    let xattr_name = "system.ima";
+fn set_xattr<P: AsRef<Path>>(path: P, xattr_name: &str, hash: &str) -> io::Result<()> {
     set(path, xattr_name, hash.as_bytes())?;
     Ok(())
 }
 
 fn process_directory<P: AsRef<Path>>(dir: P) -> io::Result<()> {
-    // Walk the directory and process files
-    for entry in WalkDir::new(dir) {
-        let entry = entry?;
-        if entry.file_type().is_file() {
-            let file_path = entry.path();
-            println!("Hashing file: {:?}", file_path);
-            
-            // Hash the file
-            match hash_file(file_path) {
-                Ok(hash) => {
-                    println!("Hash: {}", hash);
-                    
-                    // Set the extended attribute
-                    if let Err(e) = set_xattr(file_path, &hash) {
-                        eprintln!("Failed to set xattr for {:?}: {}", file_path, e);
-                    } else {
-                        println!("Extended attribute set for {:?}", file_path);
-                    }
+    // Walk the directory and collect all files into a vector
+    let files: Vec<_> = WalkDir::new(dir)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_type().is_file())
+        .collect();
+
+    // Process files in parallel using Rayon
+    files.into_par_iter().for_each(|entry| {
+        let file_path = entry.path();
+        println!("IMAHash(Name): {:?}", file_path);
+        
+        // Hash the file
+        match hash_file(file_path) {
+            Ok(hash) => {
+                println!("IMAHash(SHA512): {}", hash);
+                
+                // Set the extended attribute
+//                    let xattr_name = "system.ima"; //needs permissions 
+                let xattr_name = "user.ima"; //doesnt need permission
+                if let Err(e) = set_xattr(file_path, xattr_name, &hash) {
+                    eprintln!("Failed to set xattr for {:?}!!: {}", file_path, e);
+                } else {
+                    println!("Extended attribute set for {:?}", file_path);
                 }
-                Err(e) => eprintln!("Failed to hash file {:?}: {}", file_path, e),
             }
+            Err(e) => eprintln!("Failed to hash file {:?}!: {}", file_path, e),
         }
-    }
+    });
     Ok(())
 }
 
 fn main() -> io::Result<()> {
-    let dir = "./"; // Set the directory to scan, can be changed as needed
+    let dir = "./"; // Set the directory to scan, can be changed as needed. //TODO: read from Argv0, stdin, and -f file.txt
     process_directory(dir)
 }
