64d335d2bd6e7c80d562829d6190f364e35679ec
genBTC <genBTC@gmx.com>
2025-01-24 00:02:43 -0500

    v0.1.3 - allow input over stdin + check terminal with atty

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-//hash_and_xattr v0.11 - 2025 (c) genr8eofl @ gmx
-use std::{fs::File, io::{self, Read}, path::Path};
+//hash_and_xattr v0.13 - 2025 (c) genr8eofl @ gmx
+use std::{env, fs::File, io::{self, Read,BufRead}, path::{Path,PathBuf}};
 use sha2::{Sha512, Digest};
 use walkdir::WalkDir;
 use xattr::set;
 use rayon::prelude::*;
+use atty;
 
 fn hash_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
     let mut file = File::open(path)?;
@@ -28,28 +29,20 @@ fn set_xattr<P: AsRef<Path>>(path: P, xattr_name: &str, hash: &str) -> io::Resul
     Ok(())
 }
 
-fn process_directory<P: AsRef<Path>>(dir: P) -> io::Result<()> {
-    // Walk the directory and collect all files into a vector
-    let files: Vec<_> = WalkDir::new(dir)
-        .into_iter()
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.file_type().is_file())
-        .collect();
-
+fn process_files(files: Vec<PathBuf>) -> io::Result<()> {
     // Process files in parallel using Rayon
-    files.into_par_iter().for_each(|entry| {
-        let file_path = entry.path();
+    files.into_par_iter().for_each(|file_path| {
         println!("IMAHash(Name): {:?}", file_path);
         
         // Hash the file
-        match hash_file(file_path) {
+        match hash_file(&file_path) {
             Ok(hash) => {
                 println!("IMAHash(SHA512): {}", hash);
                 
                 // Set the extended attribute
 //                    let xattr_name = "system.ima"; //needs permissions 
                 let xattr_name = "user.ima"; //doesnt need permission
-                if let Err(e) = set_xattr(file_path, xattr_name, &hash) {
+                if let Err(e) = set_xattr(&file_path, xattr_name, &hash) {
                     eprintln!("Failed to set xattr for {:?}!!: {}", file_path, e);
                 } else {
                     println!("Extended attribute set for {:?}", file_path);
@@ -61,7 +54,48 @@ fn process_directory<P: AsRef<Path>>(dir: P) -> io::Result<()> {
     Ok(())
 }
 
+fn get_files_from_directory(dir: &str) -> Vec<PathBuf> {
+    WalkDir::new(dir)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_type().is_file())
+        .map(|entry| entry.path().to_path_buf())
+        .collect()
+}
+
+fn get_files_from_stdin() -> Vec<PathBuf> {
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+    
+    handle
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| PathBuf::from(line))
+        .collect()
+}
+
 fn main() -> io::Result<()> {
-    let dir = "./"; // Set the directory to scan, can be changed as needed. //TODO: read from Argv0, stdin, and -f file.txt
-    process_directory(dir)
+//    let dir = "./"; // Set the directory to scan, can be changed as needed. //TODO: and -f file.txt
+    // Get the command-line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Default directory, if no argument is passed
+    let dir = if args.len() > 1 {
+        &args[1]
+    } else {
+        "./" // Default to the current directory
+    };
+
+    // Determine if we should read from stdin or use a directory argument
+    let files = if atty::is(atty::Stream::Stdin) {
+        // If stdin is empty, use directory argument if provided
+        println!("IMAHash(Dir): {}", dir);
+        get_files_from_directory(dir)
+    } else {
+        // Read from stdin (piped input)
+        println!("IMAHash(stdin):");
+        get_files_from_stdin()
+    };
+    // Process the files (hash and set xattr)
+    process_files(files)
 }
