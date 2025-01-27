daacf1a4992aa27cc466e2167b21f43d2c697888
genBTC <genBTC@gmx.com>
2025-01-27 14:03:49 -0500

    Hash_Algorithm enum and working 0x0406 signature on simple ima_hash

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,139 @@
-//hash_and_xattr v0.14 - 2025 (c) genr8eofl @ gmx
-use std::{env, fs::File, io::{self, Read,BufRead}, path::{Path,PathBuf}};
-use sha2::{Sha512, Digest};
-use walkdir::WalkDir;
-use xattr::set;
-use rayon::prelude::*;
-use atty;
+use openssl::hash::{Hasher, MessageDigest};
+use openssl::nid::Nid;
+use openssl::x509::X509;
+use std::fs;
+use std::io::{self, Read};
+use std::os::unix::fs::MetadataExt;
+use std::path::Path;
 
-fn hash_file<P: AsRef<Path>>(path: P) -> io::Result<String> {
-    let mut file = File::open(path)?;
-    let mut hasher = Sha512::new();
-    let mut buffer = Vec::new();
-    
-    // Read the file in chunks to avoid loading it all into memory at once
-    while let Ok(bytes_read) = file.read_to_end(&mut buffer) {
-        if bytes_read == 0 {
-            break;
+const MAX_DIGEST_SIZE: usize = 64;
+const IMA_XATTR_DIGEST_NG: u8 = 0x04;
+
+#[derive(Debug)]
+enum HashAlgorithm {
+    Sha1,
+    Md4,
+    Md5,
+    Ripemd160,
+    Sha256,
+    Sha384,
+    Sha512,
+    Sha224,
+    Sm3_256,
+}
+
+impl HashAlgorithm {
+    fn from_str(algo: &str) -> Option<Self> {
+        match algo.to_lowercase().as_str() {
+            "sha1" => Some(HashAlgorithm::Sha1),
+            "md4" => Some(HashAlgorithm::Md4),
+            "md5" => Some(HashAlgorithm::Md5),
+            "ripemd160" => Some(HashAlgorithm::Ripemd160),
+            "sha256" => Some(HashAlgorithm::Sha256),
+            "sha384" => Some(HashAlgorithm::Sha384),
+            "sha512" => Some(HashAlgorithm::Sha512),
+            "sha224" => Some(HashAlgorithm::Sha224),
+            "sm3_256" => Some(HashAlgorithm::Sm3_256),
+            _ => None,
         }
-        hasher.update(&buffer);
-        buffer.clear();
     }
-    
-    // Finalize the hash and convert it to hex
-    Ok(format!("{:x}", hasher.finalize()))
-}
 
-fn set_xattr<P: AsRef<Path>>(path: P, xattr_name: &str, hash: &str) -> io::Result<()> {
-    set(path, xattr_name, hash.as_bytes())?;
-    Ok(())
-}
-
-fn process_files(files: Vec<PathBuf>) -> io::Result<()> {
-    // Process files in parallel using Rayon
-    files.into_par_iter().for_each(|file_path| {
-        println!("IMAHash(Name): {:?}", file_path);
-        
-        // Hash the file
-        match hash_file(&file_path) {
-            Ok(hash) => {
-                println!("IMAHash(SHA512): {}", hash);
-                
-                // Set the extended attribute
-                let xattr_name = "system.ima"; //needs permissions 
-//                let xattr_name = "user.ima"; //doesnt need permission
-                if let Err(e) = set_xattr(&file_path, xattr_name, &hash) {
-                    eprintln!("Failed to set xattr for {:?}!!: {}", file_path, e);
-                } else {
-                    println!("Extended attribute set for {:?}", file_path);
-                }
-            }
-            Err(e) => eprintln!("Failed to hash file {:?}!: {}", file_path, e),
+    fn nid(&self) -> Nid {
+        match self {
+            HashAlgorithm::Sha1 => Nid::SHA1,
+            HashAlgorithm::Md4 => Nid::MD4,
+            HashAlgorithm::Md5 => Nid::MD5,
+            HashAlgorithm::Ripemd160 => Nid::RIPEMD160,
+            HashAlgorithm::Sha224 => Nid::SHA224,
+            HashAlgorithm::Sha256 => Nid::SHA256,
+            HashAlgorithm::Sha384 => Nid::SHA384,
+            HashAlgorithm::Sha512 => Nid::SHA512,
+            HashAlgorithm::Sm3_256 => Nid::SM3,
         }
-    });
+    }
+
+    fn ima_xattr_type(&self) -> u8 {
+        match self {
+            HashAlgorithm::Sha1 => 0,
+            HashAlgorithm::Md4 => 1,
+            HashAlgorithm::Md5 => 2,
+            HashAlgorithm::Ripemd160 => 3,
+            HashAlgorithm::Sha256 => 4,
+            HashAlgorithm::Sha384 => 5,
+            HashAlgorithm::Sha512 => 6,
+            HashAlgorithm::Sha224 => 7,
+            HashAlgorithm::Sm3_256 => 8,
+        }
+    }
+}
+
+fn main() {
+    let file = "README.md";
+    let hash_algo = "sha512";
+
+    match hash_ima(file, hash_algo) {
+        Ok(_) => println!("Successfully hashed IMA"),
+        Err(e) => eprintln!("Error hashing IMA: {:?}", e),
+    }
+}
+
+fn hash_ima(file: &str, hash_algo: &str) -> io::Result<()> {
+    let mut hash = vec![0u8; MAX_DIGEST_SIZE + 2]; //64 +2 byte xattr header
+    let hash_md = HashAlgorithm::from_str(hash_algo)
+        .ok_or(io::Error::new(io::ErrorKind::InvalidInput, "Invalid hash algorithm"))?;
+    hash[0] = IMA_XATTR_DIGEST_NG;  // 0x04
+    hash[1] = hash_md.ima_xattr_type(); // 0x06
+    let offset = 2;
+
+    // Calc Hash
+    let len = calc_hash(file, &hash_md, &mut hash[offset..])?;
+    let len = len + offset;
+
+    // Print hash
+    println!("hash({}={}): {:?}", hash_algo, hash_md.ima_xattr_type(), &hash[..len]);
+
+    // Set extended attribute
+    set_xattr(file, "user.imafake", &hash[..len])
+}
+
+fn calc_hash(file: &str, hash_algo: &HashAlgorithm, hash: &mut [u8]) -> io::Result<usize> {
+    let mut file = fs::File::open(file)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+
+    let md = match MessageDigest::from_nid(hash_algo.nid()) {
+        Some(md) => md,
+        None => return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid hash algorithm")),
+    };
+
+    let mut hasher = Hasher::new(md)?;
+    hasher.update(&buffer)?;
+    let hash_result = hasher.finish()?;
+    let len = hash_result.len();
+    hash[..len].copy_from_slice(&hash_result);
+
+    Ok(len)
+}
+
+fn set_xattr(file: &str, name: &str, value: &[u8]) -> io::Result<()> {
+    use std::ffi::CString;
+    use std::os::unix::ffi::OsStrExt;
+    use std::path::Path;
+
+    let file_cstr = CString::new(Path::new(file).as_os_str().as_bytes())?;
+    let name_cstr = CString::new(name)?;
+
+    unsafe {
+        let ret = libc::setxattr(
+            file_cstr.as_ptr(),
+            name_cstr.as_ptr(),
+            value.as_ptr() as *const libc::c_void,
+            value.len(),
+            0,
+        );
+        if ret != 0 {
+            return Err(io::Error::last_os_error());
+        }
+    }
     Ok(())
-}
-
-fn get_files_from_directory(dir: &str) -> Vec<PathBuf> {
-    WalkDir::new(dir)
-        .into_iter()
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.file_type().is_file())
-        .map(|entry| entry.path().to_path_buf())
-        .collect()
-}
-
-fn get_files_from_stdin() -> Vec<PathBuf> {
-    let stdin = io::stdin();
-    let handle = stdin.lock();
-    handle.lines()
-        .filter_map(|line| line.ok())
-        .map(|line| PathBuf::from(line))
-        .collect()
-}
-
-fn get_files_from_file(file_path: &str) -> io::Result<Vec<PathBuf>> {
-    let file = File::open(file_path)?;
-    let reader = io::BufReader::new(file);
-    // Read each line, strip any surrounding whitespace, and convert it to PathBuf
-    let files: Vec<PathBuf> = reader.lines()
-        .filter_map(|line| line.ok())            // Filter out any lines that can't be read
-        .map(|line| PathBuf::from(line.trim()))  // Trim whitespace and convert to PathBuf
-        .collect();
-    Ok(files)
-}
-
-fn main() -> io::Result<()> {
-    // Get the command-line arguments
-    let args: Vec<String> = env::args().collect();
-
-    // Default directory, if no argument is passed
-    let dir = if args.len() > 1 {
-        &args[1]
-    } else {
-        "./" // Set default to the current directory
-    };
-
-    // Determine if we should read from stdin or use a directory argument
-    let files = if atty::is(atty::Stream::Stdin) {
-        // If stdin is empty, use directory argument if provided
-        println!("IMAHash(Dir): {}", dir);
-        get_files_from_directory(dir)
-    } else {
-        // Read from stdin (piped input)
-        println!("IMAHash(stdin):");
-        get_files_from_stdin()
-    };
-    // Process the files (hash and set xattr)
-    process_files(files)
 }
