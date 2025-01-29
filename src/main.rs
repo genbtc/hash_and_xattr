13eddcd3b02988be4c6d49984a2989e34be0e1a3
genBTC <genBTC@gmx.com>
2025-01-28 22:52:42 -0500

    works but value doesnt match evmctl

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,28 @@ use openssl::hash::{Hasher, MessageDigest};
 use openssl::pkey::PKey;
 use openssl::sign::Signer;
 use std::fs;
-use std::io::{self, Read};
+use std::io::{self};
 use libc;
 use std::ffi::CString;
 use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
+
 mod keyid;
 use crate::keyid::extract_keyid_from_x509_pem;
 #[allow(non_snake_case)]
 mod IMAhashAlgorithm;
 use crate::IMAhashAlgorithm::HashAlgorithm;
 
-//default _was_ sha256 https://github.com/linux-integrity/ima-evm-utils/blob/next/src/imaevm.h#L71
+//Default _was_ sha256 https://github.com/linux-integrity/ima-evm-utils/blob/next/src/imaevm.h#L71
 const DEFAULT_HASH_ALGO: &'static str = "sha512";
 //Derived from https://github.com/linux-integrity/ima-evm-utils/blob/next/src/imaevm.h#L77-L78
-const MAX_DIGEST_SIZE: usize = 64; // Adjust based on the maximum hash size
-const MAX_SIGNATURE_SIZE: usize = 512; // Adjust based on the maximum signature size
+const MAX_DIGEST_SIZE: u8 = 64; // Adjust based on the maximum hash size
+const MAX_SIGNATURE_SIZE: u16 = 512; // Adjust based on the maximum signature size
 //Derived from enum evm_ima_xattr_type @  https://github.com/linux-integrity/ima-evm-utils/blob/next/src/imaevm.h#L92-L99
 const IMA_XATTR_DIGEST: u8 = 0x01;
 const EVM_IMA_XATTR_DIGSIG: u8 = 0x03;
 const IMA_XATTR_DIGEST_NG: u8 = 0x04;
 const DIGSIG_VERSION_2: u8 = 0x02;
-
 
 fn main() {
     let targetfile = "testA"; // TODO: Replace with the actual target file path to hash
@@ -42,28 +42,35 @@ fn format_hex(bytes: &[u8]) -> String {
 }
 
 fn sign_ima(file: &str, hash_algo: HashAlgorithm, key_path: &str) -> io::Result<()> {
-    // Create Buffer
-    let header_len = MAX_DIGEST_SIZE + 2;
-    let mut header = vec![0u8; header_len]; // +2 byte xattr header (0406)
+    // Create buffer
+    let _header_len = MAX_SIGNATURE_SIZE + 9;
+    let mut header: Vec<u8> = vec![];
 
     //Calc hash
-    //HashAlgo to MessageDigest
-    let md = MessageDigest::from_nid(hash_algo.nid())
+    let md = MessageDigest::from_nid(hash_algo.nid())      //HashAlgo to MessageDigest
         .ok_or(io::Error::new(io::ErrorKind::InvalidInput, "Invalid hash algorithm"))?;
     let calc_hash = calc_hash(file, md)?;
+    let len = calc_hash.len();
+    if len < MAX_DIGEST_SIZE.into() {
+        println!{"hash len is smaller than expected MAX_DIGEST_SIZE {}", MAX_DIGEST_SIZE};
+    }
 
     // Print hash
     println!("hash({:?}): {}", hash_algo, format_hex(&calc_hash));
 
-    // Sign the hash
+    // Sign hash
     let signature = sign_hash(md, &calc_hash, key_path)?;
+    let slen = signature.len();
+    if slen < MAX_SIGNATURE_SIZE.into() {
+        println!{"signature len is smaller than expected MAX_SIGNATURE_SIZE {}", MAX_SIGNATURE_SIZE};
+    }
 
     // Start IMA Header (0406)
     if hash_algo.ima_xattr_type() > 1 {
-        header[0] = IMA_XATTR_DIGEST_NG;
-        header[1] = hash_algo.ima_xattr_type();
+        header.push(IMA_XATTR_DIGEST_NG);
+        header.push(hash_algo.ima_xattr_type());
     } else {
-        header[0] = IMA_XATTR_DIGEST;
+        header.push(IMA_XATTR_DIGEST);
     }
     let _offset = if hash_algo.ima_xattr_type() > 1 { 2 } else { 1 };
 
@@ -78,7 +85,7 @@ fn sign_ima(file: &str, hash_algo: HashAlgorithm, key_path: &str) -> io::Result<
     // Extend xattr_value with key_id vector
     match keyid_result {
         Ok(keyid_bytes) => {
-            println!("Key ID (from X509v3 SKI): {:?}", format_hex(&keyid_bytes));
+            println!("Key ID (X509v3 S.K.I.): {:?}", format_hex(&keyid_bytes));
             xattr_value.extend_from_slice(&keyid_bytes);
         }
         Err(e) => {
@@ -86,7 +93,7 @@ fn sign_ima(file: &str, hash_algo: HashAlgorithm, key_path: &str) -> io::Result<
         }
     }
     //Append max sig size (0x0200)
-    xattr_value.extend_from_slice(&MAX_SIGNATURE_SIZE.to_be_bytes()[6..8]);
+    xattr_value.extend_from_slice(&MAX_SIGNATURE_SIZE.to_be_bytes());
     //Append Signature
     xattr_value.extend_from_slice(&signature);
 
@@ -99,12 +106,10 @@ fn sign_ima(file: &str, hash_algo: HashAlgorithm, key_path: &str) -> io::Result<
 }
 
 fn calc_hash(file: &str, md: MessageDigest) -> io::Result<Vec<u8>> {
-    let mut file = fs::File::open(file)?;
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
+    let file = fs::read(file)?;
 
     let mut hasher = Hasher::new(md)?;
-    hasher.update(&buffer)?;
+    hasher.update(&file)?;
     let hash_result = hasher.finish()?;
     // Convert DigestBytes to Vec<u8>
     let hash_vec = hash_result.to_vec();
@@ -112,14 +117,15 @@ fn calc_hash(file: &str, md: MessageDigest) -> io::Result<Vec<u8>> {
     Ok(hash_vec)
 }
 
-fn sign_hash(md: MessageDigest, hash: &[u8], key_path: &str) -> io::Result<Vec<u8>> {
+fn sign_hash(md: MessageDigest, hash: &Vec<u8>, key_path: &str) -> io::Result<Vec<u8>> {
     let private_key = fs::read(key_path)?;
     let pkey = PKey::private_key_from_pem(&private_key)?;
 
-    //Pkey: PKey { algorithm: "RSA" }
-    println!("(DEBUG) {:?}", pkey);
-    //PkeyAlgorithmRSA: Ok(Rsa), PkeyBits: 4096
-    println!("(DEBUG) EVP_PKEY_get1_RSA: {:?}, EVP_PKEY_bits: {:?}, EVP_PKEY_id: {:?}, EVP_PKEY_size: {:?}", &pkey.rsa().unwrap(), &pkey.bits(), &pkey.id(), &pkey.size());
+    //(DEBUG) PKey { algorithm: "RSA" }
+//    println!("(DEBUG) {:?}", pkey);
+    //(DEBUG) EVP_PKEY_get1_RSA: Rsa, EVP_PKEY_bits: 4096, EVP_PKEY_id: Id(6), EVP_PKEY_size: 512
+//    println!("(DEBUG) EVP_PKEY_get1_RSA: {:?}, EVP_PKEY_bits: {:?}, EVP_PKEY_id: {:?}, EVP_PKEY_size: {:?}"
+//                                 , &pkey.rsa().unwrap(), &pkey.bits(), &pkey.id(), &pkey.size());
 
     let mut signer = Signer::new(md, &pkey)?;
     signer.update(hash)?;
